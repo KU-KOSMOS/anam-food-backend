2f77c5dc4927a277762698107533c7302f1491a6
BeLeap <changseo_jang@korea.ac.kr>
2020-09-05 15:11:47 +0900

    Logger by env setup

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,6 +1,32 @@
 import winston, { createLogger, format, transports } from 'winston';
 import 'winston-daily-rotate-file';
 
+const { NODE_ENV } = process.env;
+
+const transport = NODE_ENV !== 'production' ? [
+    new transports.Console({
+        format: format.combine(
+            format.colorize(),
+            format.simple()
+        )
+    })
+] : [
+    new (winston.transports.DailyRotateFile)({
+        filename: 'anam-food-backend-%DATE%.log',
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d',
+    }),
+    new (winston.transports.DailyRotateFile)({
+        filename: 'error-anam-food-backend-%DATE%.log',
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d',
+    })
+];
+
 const logger = createLogger({
     level: 'info',
     format: format.combine(
@@ -12,20 +38,7 @@ const logger = createLogger({
         format.json()
     ),
     defaultMeta: { service: 'anam-food-backend' },
-    transports: [
-        new (winston.transports.DailyRotateFile)({
-            filename: 'anam-food-backend-%DATE%.log',
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d',
-        }),
-        new (winston.transports.DailyRotateFile)({
-            filename: 'error-anam-food-backend-%DATE%.log',
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d',
-        })
-    ]
-})
+    transports: transport
+});
+
+export default logger;
