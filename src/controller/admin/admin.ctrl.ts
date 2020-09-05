c74ceda590790fd6403a5e9eba375e887197db28
BeLeap <changseo_jang@korea.ac.kr>
2020-09-05 15:59:36 +0900

    Sample controller using joi

--- src/controller/admin/admin.ctrl.ts
+++ src/controller/admin/admin.ctrl.ts
@@ -1,8 +1,16 @@
 import { RequestHandler } from 'express';
+import joi from 'joi';
+
+const postDataScheme = joi.object({
+    data: joi.string(),
+})
 
 export const postData: RequestHandler = (req, res, next) => {
+    const { error, value } = postDataScheme.validate(req.body);
     try {
-        res.json({ data: 'hi' });
+        if(error) throw new Error('NOT_FOUND');
+
+        res.json({ data: value });
     } catch (err) {
         next(err);
     }
