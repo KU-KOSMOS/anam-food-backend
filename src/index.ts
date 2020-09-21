5434b233884b14e1c2817a94501721899d964c34
BeLeap <changseo_jang@korea.ac.kr>
2020-09-21 21:07:49 +0900

    Fix Eslint Errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import server from './server';
+import logger from './lib/logger';
 
 const port = 3000;
 
 server.listen(port, () => {
-    console.log(`Server listening on ${port}`);
-})
+    logger.info(`Server listening on ${port}`);
+});
--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -3,49 +3,49 @@ import 'winston-daily-rotate-file';
 
 const { NODE_ENV } = process.env;
 
-const transport = NODE_ENV !== 'production' ? [
-    new transports.Console({
-        format: format.combine(
-            format.colorize(),
-            format.simple()
-        )
-    })
-] : [
-    new transports.DailyRotateFile({
-        filename: 'anam-food-backend-%DATE%.log',
-        datePattern: 'YYYY-MM-DD-HH',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '14d',
-    }),
-    new transports.DailyRotateFile({
-        filename: 'error-anam-food-backend-%DATE%.log',
-        datePattern: 'YYYY-MM-DD-HH',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '14d',
-    })
-];
+const transport =
+    NODE_ENV !== 'production'
+        ? [
+              new transports.Console({
+                  format: format.combine(format.colorize(), format.simple()),
+              }),
+          ]
+        : [
+              new transports.DailyRotateFile({
+                  filename: 'anam-food-backend-%DATE%.log',
+                  datePattern: 'YYYY-MM-DD-HH',
+                  zippedArchive: true,
+                  maxSize: '20m',
+                  maxFiles: '14d',
+              }),
+              new transports.DailyRotateFile({
+                  filename: 'error-anam-food-backend-%DATE%.log',
+                  datePattern: 'YYYY-MM-DD-HH',
+                  zippedArchive: true,
+                  maxSize: '20m',
+                  maxFiles: '14d',
+              }),
+          ];
 
 const logger = createLogger({
     level: 'info',
     format: format.combine(
         format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
+            format: 'YYYY-MM-DD HH:mm:ss',
         }),
         format.errors({ stack: true }),
         format.splat(),
-        format.json()
+        format.json(),
     ),
     defaultMeta: { service: 'anam-food-backend' },
     transports: transport,
-    exitOnError: false
+    exitOnError: false,
 });
 
 export const loggerStream = {
     write: (message: string) => {
         logger.info(message);
-    }
-}
+    },
+};
 
 export default logger;
