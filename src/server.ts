bc07ce69d27d6effb4d3995c872a3a03244e0988
BeLeap <changseo_jang@korea.ac.kr>
2020-09-28 21:41:24 +0900

    Establish DB connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 
-import { loggerStream } from './lib/logger';
+import { createConnection } from 'typeorm';
+
+import logger, { loggerStream } from './lib/logger';
 
 import controller from './controller';
 import errorHandler from './lib/errorHandler';
@@ -16,5 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(controller);
 app.use('/api', errorHandler);
+
+const dbInit = async () => {
+    try {
+        await createConnection();
+        logger.info('DB connection established');
+    } catch (err) {
+        logger.error(`Error occurred during DB connection: ${err}`);
+    }
+};
 
 export default app;
