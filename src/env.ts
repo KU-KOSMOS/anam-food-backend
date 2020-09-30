aec3a19d548b041fec37380c197897b76f1cbccf
BeLeap <changseo_jang@korea.ac.kr>
2020-09-30 18:07:14 +0900

    fix env

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,5 @@
-import path from 'path';
 import dotenv from 'dotenv';
 
 const envPath =
-    process.env.NODE_ENV !== 'production'
-        ? path.join(__dirname, '../.env.development')
-        : path.join(__dirname, '../.env');
+    process.env.NODE_ENV !== 'production' ? '.env.development' : '.env';
 dotenv.config({ path: envPath });
