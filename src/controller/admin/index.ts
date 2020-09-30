75d9428ecb44b5e0cfa3d5e9944ae416600c918c
BeLeap <changseo_jang@korea.ac.kr>
2020-09-30 21:41:52 +0900

    Remove unused admin placeholder

--- src/controller/admin/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { postData } from './admin.ctrl';
-
-const router = Router();
-
-router.post('/', postData);
-
-export default router;
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
-import admin from './admin';
 import store from './store';
 
 const router = Router();
 
-router.use('/admin', admin);
 router.use('/store', store);
 
 export default router;
