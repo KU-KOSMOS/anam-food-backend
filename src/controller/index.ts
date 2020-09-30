7c839bade763cf2689a75a309396b387eba303a2
BeLeap <changseo_jang@korea.ac.kr>
2020-09-30 14:10:52 +0900

    Add store import

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import admin from './admin';
+import store from './store';
 
 const router = Router();
 
 router.use('/admin', admin);
+router.use('/store', store);
 
 export default router;
